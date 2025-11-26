bfe82721f58f559c61b3751040b260db048aed52
AMagicPear <AMagicPear@outlook.com>
2025-11-27 00:12:27 +0800

    调整基本布局 确认首屏

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,26 @@
 <script lang="ts">
-  import Counter from "./lib/Counter.svelte";
-</script>
+  import LeftCard from "./lib/LeftCard.svelte";
+  </script>
 
 <main>
-  <h1>amagicpear new site test</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
+  <section id="top-showcase">
+    <!-- <h1>amagicpear new site test</h1> -->
+     <LeftCard/>
+  </section>
+  <section id="sub-content">
+    <h2>More Info</h2>
+  </section>
 </main>
 
 <style>
+  #top-showcase {
+    width: 100%;
+    height: 100vh;
+    background-color: #212420;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
 
 </style>
---
+++ src/lib/LeftCard.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+</script>
+
+<div class="left-card">
+  <h2>Left Card</h2>
+</div>
+
+<style>
+  .left-card {
+    width: 50%;
+    height: 50%;
+    background-color: rgb(144, 211, 122);
+    color: #fff;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
